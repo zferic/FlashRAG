8e804fee59a18fb0c30443bf0ecaa401e3bea248
Zlatan <zferic85@gmail.com>
2025-11-10 17:26:39 -0500

    index

--- scripts/build_index.sh
+++ scripts/build_index.sh
@@ -1,8 +1,8 @@
 CUDA_VISIBLE_DEVICES=0 python -m flashrag.retriever.index_builder \
     --retrieval_method e5 \
-    --model_path /model/e5-base-v2 \
-    --corpus_path .. \
-    --save_dir /index/ \
+    --model_path intfloat/e5-base-v2 \
+    --corpus_path /projects/nucar/feric.z/FLashRAG-z/test_sample_2018_sent.jsonl \
+    --save_dir /projects/nucar/feric.z/wikiindex2 \
     --use_fp16 \
     --max_length 256 \
     --batch_size 512 \
